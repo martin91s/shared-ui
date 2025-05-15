16846cf8b1031e714438ac7e0d789a7d5197efac
Martin Smith <martin.smith@MacBook-Pro.local>
2025-05-15 18:48:49 +0100

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,17 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  // ...
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/web-components.js'), // ✅ now inside src
+      entry: path.resolve(__dirname, 'src/web-components.js'),
       formats: ['es'],
       fileName: 'web-components',
     },
     rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
+      // ❌ remove external vue
+      external: [],
     },
   },
 });
