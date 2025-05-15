9b9ec7cf455dbecadaaf3f1296a1181c604eb1fe
Martin Smith <martin.smith@MacBook-Pro.local>
2025-05-15 21:18:34 +0100

    add styles

--- src/web-comp.js
+++ src/web-comp.js
@@ -1,6 +1,9 @@
 import { defineCustomElement } from 'vue'
 import ClientSearchCeComponent from './components/ClientSearch.ce.vue'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
 const ClientSearch = defineCustomElement(ClientSearchCeComponent);
 
 if (typeof window !== 'undefined') {
