6bb7d2f1b1792d6088d86b3ce9569464545fdc7e
Martin Smith <martin.smith@MacBook-Pro.local>
2025-05-16 09:51:58 +0100

    update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,9 @@
 /** @type { import('@storybook/vue3').Preview } */
 import { injectStyles } from './styles';
-import { setup } from '@storybook/vue3';
-import { createBootstrap } from 'bootstrap-vue-next';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 const modules = import.meta.glob('../src/components/**/*.ce.vue', { eager: true });
 
 Object.values(modules).forEach((component) => {
     injectStyles(component.default);
-});
-
-setup((app) => {
-    app.use(createBootstrap());
 });
 
 const preview = {
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
@@ -11,14 +9,11 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    Components({
-      resolvers: [BootstrapVueNextResolver()],
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: 'vue/dist/vue.esm-browser.js', // ✅ browser-safe build of Vue
+      vue: 'vue/dist/vue.esm-browser.js',
     },
   },
   build: {
